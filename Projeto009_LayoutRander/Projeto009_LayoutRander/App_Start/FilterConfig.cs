013370f84bf2fafa3c0d5ebb104d87e57fe1ca38
Jonathan <jonathanhenriquecwb@gmail.com>
2018-05-31 18:03:28 -0300

    Layout RanderPage RenderBody

---
+++ Projeto009_LayoutRander/Projeto009_LayoutRander/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto009_LayoutRander {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
